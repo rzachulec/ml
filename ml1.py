fab374edb38cafe73ede59bfa878b7e20d8f987d
Jan Narozny <rzachulec@gmail.com>
2025-01-13 23:05:40 +0100

    ml1 mnist acc0.99

---
+++ ml1.py
@@ -0,0 +1,75 @@
+#
+#  ml1.py
+#  ml
+#
+#  Created by jan on 12/01/2025.
+#
+
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+num_classes = 10
+input_shape = (28, 28, 1)
+
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# Scale images to the [0, 1] range
+x_train = x_train.astype("float32") / 255
+x_test = x_test.astype("float32") / 255
+# Make sure images have shape (28, 28, 1)
+x_train = np.expand_dims(x_train, -1)
+x_test = np.expand_dims(x_test, -1)
+print("x_train shape:", x_train.shape)
+print(x_train.shape[0], "train samples")
+print(x_test.shape[0], "test samples")
+
+
+# convert class vectors to binary class matrices
+y_train = tf.keras.utils.to_categorical(y_train, num_classes)
+y_test = tf.keras.utils.to_categorical(y_test, num_classes)
+
+model = tf.keras.Sequential(
+    [
+        tf.keras.Input(shape=input_shape),
+        tf.keras.layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        tf.keras.layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(num_classes, activation="sigmoid"),
+    ]
+)
+
+loss_fn = tf.keras.losses.CategoricalCrossentropy(from_logits=True)
+
+model.summary()
+
+model.compile(
+    optimizer="adam",
+    loss=loss_fn,
+    metrics=["acc"]
+)
+
+tf.keras.utils.plot_model(
+    model,
+    to_file="model.png",
+                          show_shapes=True,
+    show_dtype=True,
+    show_layer_names=True,
+    rankdir="TB",
+    expand_nested=False,
+    dpi=200,
+    show_layer_activations=True,
+    show_trainable=False,
+)
+
+batch_size = 128
+epochs = 5
+
+model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size)
+
+score = model.evaluate(x_test, y_test, verbose=0)
+print("Test loss:", score[0])
+print("Test accuracy:", score[1])
